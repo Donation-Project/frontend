ccb8bd62f7daa4881e4fc8cc02a1aa9e87df9ab6
WoojinDeve <jwj060111@gmail.com>
2022-10-24 16:28:21 +0900

    feat: 마이페이지 기능 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import {
 import MainPage from './component/page/MainPage';
 import LoginPage from './component/page/Member/LoginPage';
 import NewMemberPage from "./component/page/Member/NewMemberPage";
+import UserDonationList from "./component/page/Mypage/UserDonationList";
+import UserProfileEdit from "./component/page/Mypage/UserEditProfile";
 import RegistrationPage from "./component/page/RegistrationPage";
 function App(props) {
     return (
@@ -18,6 +20,8 @@ function App(props) {
                 <Route path="post-LoginPage" element={<LoginPage />} />
                 <Route path="post-NewMemberPage" element={<NewMemberPage />} />
                 <Route path="post-RegistrationPage" element={<RegistrationPage />} />
+                <Route path="mypage"  element={<UserProfileEdit />} />
+                <Route path="mypage/donate"  element={<UserDonationList />} />
             </Routes>
         </BrowserRouter>
     );
